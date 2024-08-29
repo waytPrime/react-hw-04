92035a74b120251c8a6d25b0d6f19c2ddc6ab3f7
waytPrime <123025850+waytPrime@users.noreply.github.com>
2024-08-29 13:09:43 +0300

    1

--- src/httpRequest/httpRequest.js
+++ src/httpRequest/httpRequest.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 export default function fetchRequest(query, page = 1) {
-  return axios.get(
+  const x = axios.get(
     `https://api.unsplash.com/search/photos?page=${page}&query=${query}&client_id=Mu72MXAFZvQykVVURAQpcfIFtaRdsEaBG0D-DSbuZL0`
   );
+  console.log(x);
+  return x;
 }
